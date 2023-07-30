0324724530916117cf70e58645c8b6ca518b2408
EmiTorres93 <emilianat1992@gmail.com>
2023-07-30 15:42:58 -0300

    Agrego funcionalidad del Delete

--- js/app.js
+++ js/app.js
@@ -30,16 +30,29 @@ const crearProductos = (e) => {
   cargarTabla();
 };
 
+const borrarProducto = (index) => {
+  let validar = confirm(
+    `¿Está seguro de que quiere borrar ${productos[index].nombre}?`
+  );
+
+  if (validar) {
+    productos.splice(index, 1);
+    localStorage.setItem("productos", JSON.stringify(productos));
+    cargarTabla();
+  }
+};
+
 const cargarTabla = () => {
   cuerpoTabla.innerHTML = "";
 
-  productos.forEach((producto) => {
+  productos.forEach((producto, index) => {
     const fila = document.createElement("tr");
     const celdas = `<tr>
      <th scope="row">${producto.id}</th>
      <td>${producto.nombre}</td>
      <td>${producto.detalle}</td>
      <td>${producto.precio}</td>
+     <td><button class="btn btn-danger" onClick="borrarProducto(${index})">X</button></td>
     </tr>`;
 
     fila.innerHTML = celdas;
