deeccafe527981e8afc51d0ebe660d61022b18c4
EmiTorres93 <emilianat1992@gmail.com>
2023-07-30 11:45:31 -0300

    Creo el id

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@ const productos = JSON.parse(localStorage.getItem("productos")) || [];
 const crearProductos = (e) => {
   e.preventDefault();
 
+  const id = new Date().getTime();
+
   const nombre = document.getElementById("nombreProd").value;
   const detalle = document.getElementById("detalleProd").value;
   const imagen = document.getElementById("imagenProd").value;
   const precio = document.getElementById("precioProd").value;
 
   const item = {
+    id: id,
     nombre: nombre,
     detalle: detalle,
     imagen: imagen,
